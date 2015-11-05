a2f15afb06800961ac2d3a5f2254a5326506d6b1
Anonymous Maarten <anonymous.maarten@gmail.com>
2015-11-05 18:36:38 +0100

    Fixed license header of ADUC832.h

--- ADUC832.h
+++ ADUC832.h
@@ -1,8 +1,11 @@
 /*
 =============================================================================
 Copyright (C) 2015, Anonymous Maarten
+Copyright (C) 2007, Jesus Calvino-Fraga / jesusc at ece.ubc.ca
 
 This file is part of ADuC832 demoproject.
+
+Based on ADuC84x.h, as shipped with sdcc.
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
@@ -19,36 +22,6 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 =============================================================================
 */
-/*-------------------------------------------------------------------------
-   ADuC832.h: Register Declarations for Analog Devices
-   ADuC832 (Based on datasheet Rev B, 2013 )
-
-   Based on ADuC84x.h, as shipped with sdcc.
-
-   Copyright (C) 2007, Jesus Calvino-Fraga / jesusc at ece.ubc.ca
-
-   This library is free software; you can redistribute it and/or modify it
-   under the terms of the GNU General Public License as published by the
-   Free Software Foundation; either version 2, or (at your option) any
-   later version.
-
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License 
-   along with this library; see the file COPYING. If not, write to the
-   Free Software Foundation, 51 Franklin Street, Fifth Floor, Boston,
-   MA 02110-1301, USA.
-
-   As a special exception, if you link this library with other files,
-   some of which are compiled with SDCC, to produce an executable,
-   this library does not by itself cause the resulting executable to
-   be covered by the GNU General Public License. This exception does
-   not however invalidate any other reasons why the executable file
-   might be covered by the GNU General Public License.
--------------------------------------------------------------------------*/
 
 #ifndef REG_ADUC832_H
 #define REG_ADUC832_H
