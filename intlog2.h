bb395cfb2b6ca6355ce86d5443373da636a53f34
Anonymous Maarten <anonymous.maarten@gmail.com>
2015-11-06 18:27:42 +0100

    Added integer log2 definition header (calculate log2 at compile time)

---
+++ intlog2.h
@@ -0,0 +1,125 @@
+/*
+=============================================================================
+Copyright (C) 2015, Anonymous Maarten
+
+This file is part of ADuC832 demoproject.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+=============================================================================
+*/
+#ifndef _INTLOG2_H
+#define _INTLOG2_H
+
+#undef __VALUE_TOO_LOW
+#undef __VALUE_TOO_BIG
+#define _INTLOG2_4294967296_(VAL) -2
+
+#define _INTLOG2_2147483648_(VAL) \
+    (((VAL) <= 2147483647ULL) ? 30 : _INTLOG2_4294967296_(VAL))
+
+#define _INTLOG2_1073741824_(VAL) \
+    (((VAL) <= 1073741823ULL) ? 29 : _INTLOG2_2147483648_(VAL))
+
+#define _INTLOG2_536870912_(VAL) \
+    (((VAL) <= 536870911ULL) ? 28 : _INTLOG2_1073741824_(VAL))
+
+#define _INTLOG2_268435456_(VAL) \
+    (((VAL) <= 268435455ULL) ? 27 : _INTLOG2_536870912_(VAL))
+
+#define _INTLOG2_134217728_(VAL) \
+    (((VAL) <= 134217727ULL) ? 26 : _INTLOG2_268435456_(VAL))
+
+#define _INTLOG2_67108864_(VAL) \
+    (((VAL) <= 67108863ULL) ? 25 : _INTLOG2_134217728_(VAL))
+
+#define _INTLOG2_33554432_(VAL) \
+    (((VAL) <= 33554431ULL) ? 24 : _INTLOG2_67108864_(VAL))
+
+#define _INTLOG2_16777216_(VAL) \
+    (((VAL) <= 16777215ULL) ? 23 : _INTLOG2_33554432_(VAL))
+
+#define _INTLOG2_8388608_(VAL) \
+    (((VAL) <= 8388607ULL) ? 22 : _INTLOG2_16777216_(VAL))
+
+#define _INTLOG2_4194304_(VAL) \
+    (((VAL) <= 4194303ULL) ? 21 : _INTLOG2_8388608_(VAL))
+
+#define _INTLOG2_2097152_(VAL) \
+    (((VAL) <= 2097151ULL) ? 20 : _INTLOG2_4194304_(VAL))
+
+#define _INTLOG2_1048576_(VAL) \
+    (((VAL) <= 1048575ULL) ? 19 : _INTLOG2_2097152_(VAL))
+
+#define _INTLOG2_524288_(VAL) \
+    (((VAL) <= 524287ULL) ? 18 : _INTLOG2_1048576_(VAL))
+
+#define _INTLOG2_262144_(VAL) \
+    (((VAL) <= 262143ULL) ? 17 : _INTLOG2_524288_(VAL))
+
+#define _INTLOG2_131072_(VAL) \
+    (((VAL) <= 131071ULL) ? 16 : _INTLOG2_262144_(VAL))
+
+#define _INTLOG2_65536_(VAL) \
+    (((VAL) <= 65535ULL) ? 15 : _INTLOG2_131072_(VAL))
+
+#define _INTLOG2_32768_(VAL) \
+    (((VAL) <= 32767ULL) ? 14 : _INTLOG2_65536_(VAL))
+
+#define _INTLOG2_16384_(VAL) \
+    (((VAL) <= 16383ULL) ? 13 : _INTLOG2_32768_(VAL))
+
+#define _INTLOG2_8192_(VAL) \
+    (((VAL) <= 8191ULL) ? 12 : _INTLOG2_16384_(VAL))
+
+#define _INTLOG2_4096_(VAL) \
+    (((VAL) <= 4095ULL) ? 11 : _INTLOG2_8192_(VAL))
+
+#define _INTLOG2_2048_(VAL) \
+    (((VAL) <= 2047ULL) ? 10 : _INTLOG2_4096_(VAL))
+
+#define _INTLOG2_1024_(VAL) \
+    (((VAL) <= 1023ULL) ? 9 : _INTLOG2_2048_(VAL))
+
+#define _INTLOG2_512_(VAL) \
+    (((VAL) <= 511ULL) ? 8 : _INTLOG2_1024_(VAL))
+
+#define _INTLOG2_256_(VAL) \
+    (((VAL) <= 255ULL) ? 7 : _INTLOG2_512_(VAL))
+
+#define _INTLOG2_128_(VAL) \
+    (((VAL) <= 127ULL) ? 6 : _INTLOG2_256_(VAL))
+
+#define _INTLOG2_64_(VAL) \
+    (((VAL) <= 63ULL) ? 5 : _INTLOG2_128_(VAL))
+
+#define _INTLOG2_32_(VAL) \
+    (((VAL) <= 31ULL) ? 4 : _INTLOG2_64_(VAL))
+
+#define _INTLOG2_16_(VAL) \
+    (((VAL) <= 15ULL) ? 3 : _INTLOG2_32_(VAL))
+
+#define _INTLOG2_8_(VAL) \
+    (((VAL) <= 7ULL) ? 2 : _INTLOG2_16_(VAL))
+
+#define _INTLOG2_4_(VAL) \
+    (((VAL) <= 3ULL) ? 1 : _INTLOG2_8_(VAL))
+
+#define _INTLOG2_2_(VAL) \
+    (((VAL) <= 1ULL) ? 0 : _INTLOG2_4_(VAL))
+
+#define INTLOG2(VAL) \
+     (((VAL) < 1) ? -1 : _INTLOG2_2_(VAL))
+
+#endif //_INTLOG2_H
---
+++ tools/intlog2.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+#=============================================================================
+#Copyright (C) 2015, Anonymous Maarten
+#
+#This file is part of ADuC832 demoproject.
+#
+#This program is free software; you can redistribute it and/or
+#modify it under the terms of the GNU General Public License
+#as published by the Free Software Foundation; either version 2
+#of the License, or (at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program; if not, write to the Free Software
+#Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#=============================================================================
+
+import sys
+
+if __name__ == '__main__':
+  import argparse
+  parser = argparse.ArgumentParser(description='Create C Integer log2 include file. The definition can only return log2 values of values >= 1. It will return negative values if the input is out of range.')
+  parser.add_argument(dest='nbbits', metavar='NBBITS', type=int, help='Number of bits.', default=32, nargs='?')
+  parser.add_argument('-D', dest='define', metavar='DEFINENAME', type=str, help='Name of the define', required=False, default='INTLOG2')
+  _gh=object()
+  parser.add_argument('-g', dest='guard', type=str, required=False, nargs='?', default=_gh)
+  args = parser.parse_args()
+
+  if args.guard is _gh:
+    args.guard = None
+  elif args.guard is None:
+    args.guard = '_INTLOG2_H'
+
+  output =  sys.stdout
+
+  if args.guard:
+    output.write('#ifndef {}\n'.format(args.guard))
+    output.write('#define {}\n\n'.format(args.guard))
+
+  output.write('#undef __VALUE_TOO_LOW\n')
+  output.write('#undef __VALUE_TOO_BIG\n')
+
+  output.write('#define _{}_{}_(VAL) {}\n\n'
+    .format(args.define, 1 << (args.nbbits+1), '-2'))
+
+  for exp2 in range(args.nbbits, 0, -1):
+    lowerbound = 1 << exp2
+    output.write('#define _{}_{}_(VAL) \\\n'.format(args.define, lowerbound))
+    output.write('    (((VAL) <= {}ULL) ? {} : _{}_{}_(VAL))'
+      .format(lowerbound-1, exp2 - 1, args.define, lowerbound << 1))
+    output.write('\n\n')
+
+  output.write('#define {}(VAL) \\\n'.format(args.define))
+  output.write('     (((VAL) < 1) ? {} : _{}_2_(VAL))'
+    .format('-1', args.define))
+
+  output.write('\n\n')
+
+  if args.guard:
+    output.write('#endif //{}'.format(args.guard))
+
