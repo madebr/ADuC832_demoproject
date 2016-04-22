63e45bd521f886ffb46e99dc4906f260e5b2bf55
Anonymous Maarten <anonymous.maarten@gmail.com>
2016-04-22 18:59:16 +0200

    Added util.h header

---
+++ util.h
@@ -0,0 +1,32 @@
+/*
+=============================================================================
+Copyright (C) 2015, Anonymous Maarten
+
+This file is part of ADuC832 demoproject.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+=============================================================================
+*/
+#ifndef _UTIL_H_
+#define _UTIL_H_
+
+#include <stdint.h>
+
+static inline uint16_t convert8_16(uint8_t lsb, uint8_t msb)
+{
+  return lsb | ((uint16_t) msb) << 8;
+}
+
+#endif //_UTIL_H_
